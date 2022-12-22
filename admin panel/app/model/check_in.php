f0c1639c9dce65e75ad0d98e52220739f200cdcd
Rizky Akbar Wahyu Putranto <89757057+RizkyAkb@users.noreply.github.com>
2022-12-22 16:55:48 +0700

    Delete admin panel directory

--- admin panel/app/model/check_in.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../../controller/koneksi.php';
-$booking_id = $_GET['booking_id'];
-
-$query = mysqli_query($conn, "UPDATE booking_room SET status = 'Using room' WHERE booking_id = $booking_id ");
-if ($query) {
-    header('location:../check_in.php');
-}
-?>
